2bb4ae506df867bdc65a26474ab18deced9604db
rrLero <roman@xlan.net.ua>
2017-06-23 13:24:11 +0300

    "some corrections"

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ function LunchCheckController ($scope) {
   $scope.array = ""
 
   $scope.checkIftoMuch = function () {
+    console.log($scope.array.split(','));
     var wordAmmount = $scope.array.split(',')
-                               .filter(function(el) {return el})
+                               .filter(function(el) {
+                                 return el.trim()
+                               })
                                .length
+    console.log(wordAmmount);
     if (wordAmmount > 3) {
       $scope.message = "Too much!"
       $scope.color = "green";
